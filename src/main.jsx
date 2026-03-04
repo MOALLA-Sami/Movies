3f7df288a44260284d39c318c40debf1503988bb
Sami <moalla.sami.123@gmail.com>
2026-03-04 10:39:53 +0100

    n8n-commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import "./css/index.css"
-//test
+//testn8n
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
